3b14b075a5b7021c77f57887c3bc2226342b94d1
Vlad Hrybok <vgribok@hotmail.com>
2020-07-30 12:01:37 -0400

    Created client WebUI MVC app.

---
+++ WebUIApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebUIApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
